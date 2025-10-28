7e4c059f3e6fd3e561cf92e0fab74e08460cf9f1
chenzhigang <979703986@qq.com>
2025-10-28 17:59:59 +0800

    chore: 更新页面

--- packages/auto-import-resolver/src/index.ts
+++
@@ -1,6 +0,0 @@
-export {}
-
-const a = {}
-const b = Object.assign(a, { x: 1 })
-
-export default b
---
+++ packages/resolver/src/index.ts
@@ -0,0 +1,57 @@
+function kebabCase(key: string) {
+  const result = key.replace(/([A-Z])/g, ' $1').trim()
+  return result.split(' ').join('-').toLowerCase()
+}
+
+export interface VroResolverOptions {
+  /**
+   * 是否自动导入组件的相应样式
+   *
+   * @default true
+   */
+  importStyle?: boolean
+}
+
+function getSideEffects(options: {
+  packageName: string
+  componentName: string
+  importStyle?: boolean
+}) {
+  const { importStyle = true, packageName, componentName } = options
+  if (!importStyle) {
+    return
+  }
+  return `@vrojs/${packageName}/src/${kebabCase(componentName)}/style/index`
+}
+
+export function VroResolver(options: VroResolverOptions = {}) {
+  const { importStyle } = options
+
+  return {
+    type: 'component' as const,
+    resolve: (name: string) => {
+      if (name.startsWith('VroEl')) {
+        const packageName = '@vrojs/element-plus'
+        return {
+          name,
+          from: packageName,
+          sideEffects: getSideEffects({ importStyle, packageName, componentName: name }),
+        }
+      } else if (name.startsWith('VroVan')) {
+        const packageName = '@vrojs/vant'
+        return {
+          name,
+          from: '@vrojs/vant',
+          sideEffects: getSideEffects({ importStyle, packageName, componentName: name }),
+        }
+      } else if (name.startsWith('Vro')) {
+        const packageName = '@vrojs/core'
+        return {
+          name,
+          from: '@vrojs/core',
+          sideEffects: getSideEffects({ importStyle, packageName, componentName: name }),
+        }
+      }
+    },
+  }
+}
