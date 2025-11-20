4bbca2cd22510574faf6356c2168f6ab17b9322f
chenzhigang <979703986@qq.com>
2025-11-20 13:32:13 +0800

    chore: 更新

--- playground/src/demo/App.vue
+++ playground/src/demo/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
     <h1>112312321</h1>
+
+    <a :id href=""></a>
+
+    <button :disabled="id === '1'"></button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const id = '1'
+</script>
 
 <style lang="scss" scoped>
   // @use '@/assets/scss/define.scss' as *;
