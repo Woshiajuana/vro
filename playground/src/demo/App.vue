61cd01c5201331c592ba026d07efca505e8c5321
chenzhigang <979703986@qq.com>
2025-11-24 11:41:44 +0800

    chore: 更新页面

--- playground/src/demo/App.vue
+++ playground/src/demo/App.vue
@@ -1,15 +1,50 @@
 <template>
   <div>
-    <h1>112312321</h1>
+    <h2>Parent</h2>
 
-    <a :id href=""></a>
+    <ul>
+      <li>{{ count }}</li>
+      <li><button @click="count++">+1</button></li>
+      <li>{{ Date.now() }}</li>
 
-    <button :disabled="id === '1'"></button>
+      <li>
+        <ChildA></ChildA>
+      </li>
+      <li>
+        <ChildB></ChildB>
+      </li>
+    </ul>
+
+    <dl>
+      <dt>测试Watch</dt>
+      <dd>{{ obj.count }}</dd>
+      <dd><button @click="obj.count++">+1</button></dd>
+    </dl>
   </div>
 </template>
 
 <script setup lang="ts">
-  const id = '1'
+  import { reactive, ref, watch } from 'vue'
+
+  import ChildA from './ChildA.vue'
+  import ChildB from './ChildB.vue'
+
+  const count = ref(0)
+
+  const obj = reactive({ count: 0 })
+
+  watch(
+    () => obj.count,
+    (newValue, oldValue) => {
+      console.log('newValue => ', newValue)
+      console.log('oldValue => ', oldValue)
+      // 在嵌套的属性变更时触发
+      // 注意：`newValue` 此处和 `oldValue` 是相等的
+      // 因为它们是同一个对象！
+    },
+  )
+
+  obj.count++
 </script>
 
 <style lang="scss" scoped>
---
+++ playground/src/demo/ChildA.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>ChildA</h2>
+
+    <ul>
+      <li>{{ count }}</li>
+      <li><button @click="count++">+1</button></li>
+      <li>{{ Date.now() }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  const count = ref(0)
+</script>
+
+<style lang="scss" scoped>
+  // @use '@/assets/scss/define.scss' as *;
+</style>
