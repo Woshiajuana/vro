1610d1d03af996cdf4484212b6fe88976a00aecb
chenzhigang <979703986@qq.com>
2026-06-09 15:35:48 +0800

    chore: 更新

--- packages/element-plus/src/vro-el-icon/types.ts
+++ packages/element-plus/src/vro-el-icon/types.ts
@@ -1,5 +1,12 @@
-import type { ExtractPropTypes } from 'vue'
+import { iconProps } from 'element-plus'
+import type { ExtractPropTypes, PropType, Raw } from 'vue'
 
-export const vroElIconProps = {}
+export const vroElIconProps = {
+  ...iconProps,
+  name: {
+    type: [String, Object] as PropType<string | Raw<object>>,
+    default: '',
+  },
+}
 
 export type VroElIconProps = ExtractPropTypes<typeof vroElIconProps>
--- packages/element-plus/src/vro-el-icon/vro-el-icon.vue
+++ packages/element-plus/src/vro-el-icon/vro-el-icon.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="vro-el-icon">
-    <span>vro-el-icon</span>
-  </div>
+  <ElIcon class="vro-el-icon" v-bind="props">
+    <slot>
+      <Component v-if="isElement" :is="name" />
+      <i v-else class="iconfont" :class="[name]"></i>
+    </slot>
+  </ElIcon>
 </template>
 
 <script setup lang="ts">
+  import { isString } from '@daysnap/utils'
+  import { ElIcon } from 'element-plus'
+  import { computed } from 'vue'
+
   import { vroElIconProps } from './types'
 
-  defineOptions({ name: 'VroElIcon' })
+  const props = defineProps(vroElIconProps)
 
-  defineProps(vroElIconProps)
+  const isElement = computed(() => {
+    const { name } = props
+    if (isString(name)) {
+      const char = name.substring(0, 1)
+      return char >= 'A' && char <= 'Z'
+    }
+    return true
+  })
 </script>
