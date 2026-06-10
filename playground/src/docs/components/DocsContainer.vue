da1b241021d3aa012fe223a65a31b6dc91876d2c
chenzhigang <979703986@qq.com>
2026-06-10 14:44:57 +0800

    chore: 主题布局框架

--- playground/src/docs/components/DocsContainer.vue
+++ playground/src/docs/components/DocsContainer.vue
@@ -1,17 +1,13 @@
 <template>
   <main class="docs-container has-sidebar has-header">
-    <ElSplitter>
-      <ElSplitterPanel>
-        <RouterView class="docs-content" />
-      </ElSplitterPanel>
-      <ElSplitterPanel>
-        <slot></slot>
-      </ElSplitterPanel>
-    </ElSplitter>
+    <RouterView class="docs-content" />
+    <DemoPlayground class="docs-playground" />
   </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import DemoPlayground from './DemoPlayground.vue'
+</script>
 
 <style lang="scss" scoped>
   @use '@/assets/scss/define.scss' as *;
@@ -26,5 +22,13 @@
     &.has-header {
       padding-top: var(--header-height);
     }
+  }
+  .docs-playground {
+    position: fixed;
+    background-color: red;
+    top: 0;
+    right: 30px;
+    width: 300px;
+    bottom: 30px;
   }
 </style>
