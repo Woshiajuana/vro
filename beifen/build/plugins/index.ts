a55a01afcfcd6b6b249e62c780b54161b54e8b87
chenzhigang <979703986@qq.com>
2026-06-11 11:42:49 +0800

    chore: 删除备份文件夹

--- beifen/build/plugins/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './genShared'
-export * from './markdown'
--- beifen/src/desktop/main.js
+++
@@ -1,9 +0,0 @@
-import 'vant/es/toast/style'
-import 'vant/es/dialog/style'
-
-import { createApp } from 'vue'
-
-import App from './App.vue'
-import { router } from './router.js'
-
-createApp(App).use(router).mount('#app')
