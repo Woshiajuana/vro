8d84f3f0ef47efdff504b9ac9c82f73d7eec7637
chenzhigang <979703986@qq.com>
2026-06-21 22:49:44 +0800

    chore: 更新

--- packages/element-plus/src/vro-el-schema-form-dialog/style/css.ts
+++ packages/element-plus/src/vro-el-schema-form-dialog/style/css.ts
@@ -1,3 +1,4 @@
 import '../../style/base.scss'
 import '../../vro-el-schema-form/style/css'
+import '../../vro-el-with-config/style/css'
 import './index.scss'
--- packages/element-plus/src/vro-el-schema-form-dialog/style/deps.ts
+++ packages/element-plus/src/vro-el-schema-form-dialog/style/deps.ts
@@ -1,3 +1,4 @@
 import '../../vro-el-schema-form/style/deps'
+import '../../vro-el-with-config/style/deps'
 import 'element-plus/es/components/button/style/css'
 import 'element-plus/es/components/dialog/style/css'
