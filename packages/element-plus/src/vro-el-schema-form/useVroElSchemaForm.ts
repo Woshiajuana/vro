67462570aa9ffaa503b31eae4d8bf182d07775dc
chenzhigang <979703986@qq.com>
2026-06-23 14:45:41 +0800

    chore: 更新

--- packages/element-plus/src/vro-el-schema-form/useVroElSchemaForm.ts
+++ packages/element-plus/src/vro-el-schema-form/useVroElSchemaForm.ts
@@ -1,25 +1,28 @@
 import banana from '@daysnap/banana'
 import { isFunction } from '@daysnap/utils'
-import { useAsyncTask } from '@vrojs/use'
+import { useAsyncTask, type UseAsyncTaskOptions } from '@vrojs/use'
 import { reactive, type Ref, ref } from 'vue'
 
 import type { VroElSchemaFormInstance } from '.'
 import type { VroElSchemaFormSchema } from './types'
 
-export function useVroElSchemaForm<T extends (data: any) => Promise<any>>(
+export interface UseVroElSchemaFormOptions
+  extends Pick<UseAsyncTaskOptions<any>, 'throwError' | 'onError'> {
+  source?: Record<string, any>
+  instanceRef?: Ref<VroElSchemaFormInstance | undefined>
+}
+
+export function useVroElSchemaForm<T extends Record<string, any> = Record<string, any>>(
   rawSchema: (() => VroElSchemaFormSchema) | VroElSchemaFormSchema,
-  task?: T,
-  options: {
-    initialValue?: Record<string, any>
-    instanceRef?: Ref<VroElSchemaFormInstance | undefined>
-  } = {},
+  task?: (data: T) => Promise<void>,
+  options: UseVroElSchemaFormOptions = {},
 ) {
-  const { instanceRef, initialValue } = options
+  const { instanceRef, source, ...rest } = options
   const schema = reactive(isFunction(rawSchema) ? rawSchema() : rawSchema)
 
   const refVroElSchemaForm = instanceRef ?? ref<VroElSchemaFormInstance>()
 
-  const { loading, trigger } = useAsyncTask(
+  const { loading, trigger, error, data } = useAsyncTask(
     async () => {
       if (!refVroElSchemaForm.value) {
         return
@@ -30,16 +33,18 @@ export function useVroElSchemaForm<T extends (data: any) => Promise<any>>(
         throw ''
       }
       const data = await refVroElSchemaForm.value.extractValues()
-      await task?.(data)
+      await task?.(data as T)
     },
-    { throwError: true },
+    { throwError: true, ...rest },
   )
 
-  if (initialValue) {
-    banana.assignment(initialValue, schema as any)
+  if (source) {
+    banana.assignment(source, schema as any)
   }
 
   return {
+    data,
+    error,
     schema,
     refVroElSchemaForm,
     loading,
