380c2bb731e23e6476e1b43790adbe303e819efd
chenzhigang <979703986@qq.com>
2026-06-29 16:39:24 +0800

    chore: 0.0.4

--- packages/element-plus/src/vro-el-image-upload/style/deps.ts
+++ packages/element-plus/src/vro-el-image-upload/style/deps.ts
@@ -1,5 +1,4 @@
 import '../../vro-el-image/style/deps'
 import 'element-plus/es/components/checkbox/style/css'
 import 'element-plus/es/components/icon/style/css'
-import 'element-plus/es/components/image/style/css'
 import 'element-plus/es/components/loading/style/css'
