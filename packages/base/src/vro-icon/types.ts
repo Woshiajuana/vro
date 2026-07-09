8b90d7f715cb45880bab8e9ab998e2a20161e119
chenzhigang <979703986@qq.com>
2026-07-09 13:46:29 +0800

    chore: 升级 utils 依赖版本

--- packages/base/src/vro-icon/types.ts
+++ packages/base/src/vro-icon/types.ts
@@ -1,9 +1,18 @@
 import type { ExtractPropTypes } from 'vue'
 
 export const vroIconProps = {
-  prefix: {
+  name: String,
+  fontFamily: {
     type: String,
     default: 'iconfont',
+  },
+  color: {
+    type: String,
+    default: 'inherit',
+  },
+  size: {
+    type: [String, Number],
+    default: 'inherit',
   },
 }
 
--- packages/base/src/vro-icon/vro-icon.vue
+++ packages/base/src/vro-icon/vro-icon.vue
@@ -1,13 +1,28 @@
 <template>
-  <div class="vro-icon">
-    <span>vro-icon</span>
-  </div>
+  <i class="vro-icon" :class="classes" :style="">
+    <slot>
+      <img v-if="isImage" class="vor-icon-image" />
+    </slot>
+  </i>
 </template>
 
 <script setup lang="ts">
+  import { computed, type HTMLAttributes } from 'vue'
+
   import { vroIconProps } from './types'
 
   defineOptions({ name: 'VroIcon' })
+  const props = defineProps(vroIconProps)
 
-  defineProps(vroIconProps)
+  const isImage = computed(() => props.name?.includes('/') ?? false)
+
+  const classes = computed<HTMLAttributes['class']>(() => {
+    const { fontFamily, name } = props
+    return isImage.value ? [] : [fontFamily, name]
+  })
+
+  const style = computed<HTMLAttributes['style']>(() => {
+    const { color } = props
+    return { color, fontSize: }
+  })
 </script>
