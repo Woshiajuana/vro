cc6ce02056c8feee49ee4012561aed71e8fb3ac4
chenzhigang <979703986@qq.com>
2026-07-16 09:40:27 +0800

    chore: vro-van-cell

--- packages/vant/src/vro-van-cell/demo/index.vue
+++ packages/vant/src/vro-van-cell/demo/index.vue
@@ -3,17 +3,12 @@
     <van-cell title="11"></van-cell>
     <vro-van-cell label="单元格" value="内容" />
     <vro-van-cell label="单元格" value="内容" arrow />
-    <vro-van-cell
-      required
-      label="单元格"
-      value="内容"
-      :icon-props="{ name: 'van-icon-setting-o' }"
-      arrow
-    />
+    <vro-van-cell label="单元格" value="内容" arrow :icon-props="{ name: 'van-icon-setting-o' }" />
+    <vro-van-cell required label="单元格" value="内容" arrow />
   </demo-block>
 
   <demo-block title="内容多行">
-    <vro-van-cell label="单元格" lineClamp="2" value="1内容内容内容内容内容内容内容内1内2" />
+    <vro-van-cell label="单元格" line-clump="2" value="1内容内容内容内容内容内容内容内1内2" />
     <vro-van-cell
       label="可省略"
       value="1内容内容内容内容内容内容内容内容内容内容内容内容2"
@@ -21,7 +16,7 @@
     />
     <vro-van-cell
       label="多行可省略"
-      lineClamp="2"
+      line-clump="2"
       value="1内容内容内容内容内容内容内容内1内容内容内容内容内容内容内容内容内容内容内容内容2容内容内容内容内容2"
     />
   </demo-block>
@@ -46,12 +41,12 @@
   <demo-block title="使用插槽">
     <vro-van-cell label="单元格">
       <b>插槽内容</b>
-      <hor-icon name="hor-version" />
+      <vro-van-icon name="van-icon-setting-o" />
     </vro-van-cell>
     <vro-van-cell label="单元格" arrow>
       <input type="text" placeholder="请输入密码" />
       <template #suffix>
-        <hor-icon name="plus" />
+        <vro-van-icon name="van-icon-plus" />
       </template>
       <template #after>
         <div style="width: 100%">123</div>
