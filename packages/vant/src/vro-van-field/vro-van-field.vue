dc6c209bb0a5ab82c48dc87c2f82ecceb7d0e3ec
chenzhigang <979703986@qq.com>
2026-07-19 08:15:03 +0800

    chore: 更新

--- packages/vant/src/vro-van-field/vro-van-field.vue
+++ packages/vant/src/vro-van-field/vro-van-field.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="vro-van-field">
-    <span>vro-van-field</span>
-  </div>
+  <vro-van-cell>
+    <vro-input />
+    <vro-van-icon name="van-icon-clear" />
+  </vro-van-cell>
 </template>
 
 <script setup lang="ts">
+  import { VroInput } from '@vrojs/base'
+
+  import { VroVanCell } from '../vro-van-cell'
+  import { VroVanIcon } from '../vro-van-icon'
   import { vroVanFieldProps } from './types'
 
   defineOptions({ name: 'VroVanField' })
