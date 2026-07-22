058f836cede78af31c5e248b28cb98ebe25e0bcc
chenzhigang <979703986@qq.com>
2026-07-22 13:55:18 +0800

    chore: 案例

--- packages/vant/src/vro-van-field/demo/index.vue
+++ packages/vant/src/vro-van-field/demo/index.vue
@@ -10,6 +10,7 @@
       type="decimal"
       label="金额"
       placeholder="请输入金额"
+      clearable
       unit="元"
     />
     <vro-van-field
@@ -33,9 +34,6 @@
     />
     <vro-van-field v-model="address" direction="column" label="详细地址" placeholder="请输入地址" />
     <vro-van-field v-model="readonlyValue" label="只读字段" readonly arrow />
-    <vro-van-field>
-      <template #append></template>
-    </vro-van-field>
   </demo-block>
 </template>
 
