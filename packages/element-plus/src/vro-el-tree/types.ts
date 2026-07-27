386b2ed57c23f4a8bd4fcfa5054b7aeaa13758f0
chenzhigang <979703986@qq.com>
2026-07-27 13:56:51 +0800

    fix: el-tree

--- packages/element-plus/src/vro-el-tree/types.ts
+++ packages/element-plus/src/vro-el-tree/types.ts
@@ -53,10 +53,7 @@ export const vroElTreeProps = {
   /**
    * 树节点数据，节点需要包含 id，可通过 parentId 表示父级关系。
    */
-  options: {
-    type: Array as PropType<VroElTreeOption[]>,
-    default: () => [],
-  },
+  options: Array as PropType<VroElTreeOption[]>,
 
   /**
    * 根节点 id，用于判断顶层节点的选中链路。
