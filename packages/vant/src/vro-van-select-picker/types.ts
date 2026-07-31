2333686f59c3699ba2d3fc9c0c982491c60ab761
chenzhigang <979703986@qq.com>
2026-07-31 11:08:27 +0800

    chore: 更新

--- packages/vant/src/vro-van-select-picker/types.ts
+++ packages/vant/src/vro-van-select-picker/types.ts
@@ -1,5 +1,25 @@
-import type { ExtractPropTypes } from 'vue'
+import { omit } from '@daysnap/utils'
+import type { PickerColumn } from 'vant'
+import type { ExtractPropTypes, PropType } from 'vue'
 
-export const vroVanSelectPickerProps = {}
+import { vroVanPickerProps } from '../vro-van-picker'
+import { vroVanTriggerCellProps } from '../vro-van-trigger-cell'
+
+export const vroVanSelectPickerPickerProps = omit(vroVanPickerProps, ['modelValue', 'columns'])
+
+export const vroVanSelectPickerProps = {
+  ...vroVanTriggerCellProps,
+
+  ...vroVanSelectPickerPickerProps,
+
+  trigger: String as PropType<'immediately' | 'lazy'>,
+
+  options: {
+    type: [Array, Function] as PropType<
+      PickerColumn | (() => Promise<PickerColumn> | PickerColumn)
+    >,
+    default: () => [],
+  },
+}
 
 export type VroVanSelectPickerProps = ExtractPropTypes<typeof vroVanSelectPickerProps>
--- packages/vant/src/vro-van-select-picker/vro-van-select-picker.vue
+++ packages/vant/src/vro-van-select-picker/vro-van-select-picker.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="vro-van-select-picker">
-    <span>vro-van-select-picker</span>
-  </div>
+  <VroVanTriggerCell @click="handleSelect" />
 </template>
 
 <script setup lang="ts">
+  import { showVroVanPicker } from '../vro-van-picker'
+  import { VroVanTriggerCell } from '../vro-van-trigger-cell'
   import { vroVanSelectPickerProps } from './types'
 
   defineOptions({ name: 'VroVanSelectPicker' })
 
   defineProps(vroVanSelectPickerProps)
+
+  const handleSelect = async () => {
+    showVroVanPicker({})
+  }
 </script>
