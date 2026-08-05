3161bf787dc105836f644f7843fb166d83544851
chenzhigang <979703986@qq.com>
2026-08-05 16:30:18 +0800

    chore: image

--- packages/vant/src/vro-van-image/types.ts
+++ packages/vant/src/vro-van-image/types.ts
@@ -1,5 +1,39 @@
-import type { ExtractPropTypes } from 'vue'
+import type { ExtractPropTypes, PropType } from 'vue'
 
-export const vroVanImageProps = {}
+export type VroVanImageNormalizeSrc = (options: {
+  src: string
+  ratio?: string | number
+  baseUrl?: string
+}) => string
+
+export const vroVanImageProps = {
+  src: String,
+
+  alt: String,
+
+  // 加载失败占位图
+  placeholder: String,
+
+  // loading 图 // 默认使用 img-loading.gif
+  loading: String,
+
+  /**
+   * 图片基础地址，会传入 normalizeSrc 统一处理。
+   */
+  baseUrl: String,
+
+  /**
+   * 图片地址转换比例。
+   */
+  ratio: [String, Number],
+
+  /**
+   * 预览图片地址转换比例，默认使用 ratio。
+   */
+  previewRatio: [String, Number],
+
+  // 图片地址转换方法。
+  normalizeSrc: Function as PropType<VroVanImageNormalizeSrc>,
+}
 
 export type VroVanImageProps = ExtractPropTypes<typeof vroVanImageProps>
