63e1c6b561d990b82188beab03e6963cbf23fb22
chenzhigang <979703986@qq.com>
2026-08-07 09:37:38 +0800

    chore: 更新

--- packages/vant/src/vro-van-image-uploader/vro-van-image-uploader.vue
+++ packages/vant/src/vro-van-image-uploader/vro-van-image-uploader.vue
@@ -1,14 +1,34 @@
 <template>
   <VroVanCell class="vro-van-image-uploader" direction="column">
     <div class="vro-van-image-uploader-content">
-      <div class="vro-van-image-uploader-item">
+      <div class="vro-van-image-uploader-item is-required">
         <div class="vro-van-image-uploader-item-box">
           <VroVanIcon name="van-icon-plus" />
+          <input type="file" />
         </div>
+        <span class="vro-van-image-uploader-item-label">身份证反面</span>
+      </div>
+      <div class="vro-van-image-uploader-item is-required">
+        <div class="vro-van-image-uploader-item-box">
+          <VroVanIcon class="vro-van-image-uploader-item-plus" name="van-icon-plus" />
+          <input type="file" />
+          <VroVanIcon class="vro-van-image-uploader-item-delete" name="van-icon-cross" />
+        </div>
+        <span class="vro-van-image-uploader-item-label">身份证正面</span>
+      </div>
+      <div class="vro-van-image-uploader-item is-required">
+        <div class="vro-van-image-uploader-item-box">
+          <VroVanIcon class="vro-van-image-uploader-item-plus" name="van-icon-plus" />
+          <input type="file" />
+          <VroVanImage />
+          <VroVanIcon class="vro-van-image-uploader-item-delete" name="van-icon-cross" />
+        </div>
+        <span class="vro-van-image-uploader-item-label">身份证正面</span>
       </div>
       <div class="vro-van-image-uploader-item">
         <div class="vro-van-image-uploader-item-box">
           <VroVanIcon name="van-icon-plus" />
+          <input type="file" />
         </div>
       </div>
     </div>
@@ -18,6 +38,7 @@
 <script setup lang="ts">
   import { VroVanCell } from '../vro-van-cell'
   import { VroVanIcon } from '../vro-van-icon'
+  import { VroVanImage } from '../vro-van-image'
   import { vroVanImageUploaderProps } from './types'
 
   defineOptions({ name: 'VroVanImageUploader' })
